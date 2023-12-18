927b6344b798014a81b55f4abd138c460d7598c6
JeGwan <emperorvv@gmail.com>
2023-12-19 02:50:24 +0900

    feat: Add Button component

---
+++ dist/Button.d.ts
@@ -0,0 +1,6 @@
+import React, { ReactNode } from 'react';
+interface ButtonProps {
+    children?: ReactNode;
+}
+declare const Button: ({ children }: ButtonProps) => React.JSX.Element;
+export default Button;
